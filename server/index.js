8d9d5a3d33fefd220ab076ef8c6759f2b4eb862a
Kapil As <askapil07@gmail.com>
2024-02-17 18:24:35 +0530

    update

--- server/index.js
+++ server/index.js
@@ -1,54 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
-const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
-const session = require("express-session"); // Change this line
-const passport = require("./Middlewares/passport");
-const LocalStrategy = require("passport-local").Strategy;
-const User = require("./Model/user");
+const port = process.env.PORT;
 const db = require("./db");
 
 const app = express();
-const port = process.env.PORT;
 
-// Middleware
-app.use(helmet());
-app.use(
-  cors({
-    credentials: true,
-    origin: process.env.ORIGIN,
-  })
-);
+//middleware
 app.use(express.json());
-app.use(
-  session({
-    secret: process.env.JWT_SECRET,
-    resave: true,
-    saveUninitialized: true,
-  })
-);
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
-app.use(passport.initialize());
-app.use(passport.session());
 
-// Passport Configuration
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+app.use("/", require("./routes/auth"));
 
-// Additional middleware to handle CORS headers for redirect response
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
-  res.header("Access-Control-Allow-Credentials", true);
-  next();
-});
-
-// Routes
-app.use("/", require("./Routes/index"));
-
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
 });
